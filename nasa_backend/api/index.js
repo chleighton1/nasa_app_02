8ddba87c26b9a8458f16546e93b78e2a31c01715
Charles Leighton <chleighton1@gmail.com>
2023-06-29 18:52:12 -0400

    set cors headers in app

--- nasa_backend/api/index.js
+++ nasa_backend/api/index.js
@@ -4,12 +4,37 @@ const userRoute = require("../routes/User");
 const authRouter = require("../routes/oauth");
 const requestRouter = require("../routes/request");
 
+const whitelist = [
+  '*'
+];
+
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
+
+
 app.use(userRoute);
 app.use("/oauth", authRouter);
 app.use("/request", requestRouter);
 
 app.use(express.json());
-// app.use(cors());
+
 
 if (process.env.PORT) {
   app.listen(process.env.PORT, () => {
