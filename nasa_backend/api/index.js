167e7e669bb51fb04621c8fcdbe1bca5baf62fea
Charles Leighton <chleighton1@gmail.com>
2023-06-29 19:09:18 -0400

    removed cors errors

--- nasa_backend/api/index.js
+++ nasa_backend/api/index.js
@@ -4,29 +4,7 @@ const userRoute = require("../routes/User");
 const authRouter = require("../routes/oauth");
 const requestRouter = require("../routes/request");
 
-const whitelist = [
-  '*'
-];
 
-app.use((req, res, next) => {
-  const origin = req.get('referer');
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
-  // Pass to next layer of middleware
-  if (req.method === 'OPTIONS') res.sendStatus(200);
-  else next();
-});
-
-const setContext = (req, res, next) => {
-  if (!req.context) req.context = {};
-  next();
-};
-app.use(setContext);
 
 
 app.use(userRoute);
