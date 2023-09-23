32c99891ddb158842f2e83e571abe20dbe9b9aef
Charles Leighton <chleighton1@gmail.com>
2023-09-23 17:53:56 -0400

    test push

--- nasa_backend/routes/oauth.ts
+++ nasa_backend/routes/oauth.ts
@@ -7,7 +7,6 @@ dotenv.config();
 
 const router = Router();
 
-
 async function getUserData(access_token: string) {
   const response = await fetch(
     `https://www.googleapis.com/oauth2/v3/userinfo?access_token${access_token}`
